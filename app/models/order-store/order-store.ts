57551cbe91c435b26dbc03731752b6760e99d3dd
doanbv1 <133104362+doanbv1@users.noreply.github.com>
2024-06-07 15:54:00 +0700

    "sua ten bien"

--- app/models/order-store/order-store.ts
+++ app/models/order-store/order-store.ts
@@ -2,11 +2,6 @@ import { flow, types } from "mobx-state-tree";
 import { withEnvironment } from "../extensions/with-environment";
 import { InputSelectModel, OrderResult } from "./order-store-model";
 import { OrderApi } from "../../services/api/api_oder_screen";
-import { SelectClienAPI } from "../../services/api/api_selectClient";
-import { OderListResspose } from "../order-list-select-clien-model";
-import { AddClientAPI } from "../../services/api/api-add-client";
-import { translate } from "i18n-js";
-import { Dialog } from "../../components/dialog-notification";
 import { OrderProductResult } from "./order-product-model";
 import { OrderVariantResult } from "./order-variant-model";
 import { VendorApi } from "../../services/api/api-vendor";
@@ -19,9 +14,6 @@ export const OrderStoreModel = types
     fatherStatus: types.optional(types.frozen<InputSelectModel>(), { id: '', label: '' }),
     dataChildStatus: types.optional(types.array(types.frozen<InputSelectModel>()), []),
     childStatus: types.optional(types.frozen<InputSelectModel>(), { id: '', label: '' }),
-    sortCreateClient: types.optional(types.string,''),
-    search: types.optional(types.string,''),
-    
     dataProductAddOrder: types.optional(types.array(types.frozen<never>()), []),
     dataProductAddOrderNew: types.optional(types.array(types.frozen<never>()), []),
     checkPriceList: types.optional(types.boolean, false),
@@ -47,14 +39,6 @@ export const OrderStoreModel = types
     closeModalTracking() {
       self.isModalTracking = false;
     },
-
-    setSearch(search: any) {
-      self.search = search
-    },
-    setSortCreateClient( sort: any) {
-      self.sortCreateClient = sort
-    },
-
     setViewProductType (viewProductType : string) {
       self.viewProductType = viewProductType
     },
@@ -104,28 +88,6 @@ export const OrderStoreModel = types
         return result;
       }
     }),
-
-    getListSelectClient: flow(function * (page: number, size: number, sort: string , search: string ) {
-      try {
-        const clientAPI = new SelectClienAPI(self.environment.apiErp)
-        const result: BaseResponse<OderListResspose, ErrorCode> = yield clientAPI.getListSelectClient(page,size, sort, search)
-        console.log("SlectClientResult-------------",JSON.stringify(result.data))
-        return result.data
-      } catch (error) {
-        console.log("Get list info company", error)
-      }
-    }),
-
-    postClient: flow(function * (clientData){
-      const client = new AddClientAPI(self.environment.apiErp)
-      const result = yield client.createClient(clientData)
-      if (result.kind === 'ok') {
-        return result
-      } else {
-        return result
-    }
-    }),
-    
     getListOrderProduct: flow(function* (
       page: number,
       size: number,
