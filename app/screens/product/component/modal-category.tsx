60a69e6f97e16168e6d68f9daac728d3ec2047e1
tridung239 <damtridung2309@gmail.com>
2024-06-18 14:30:12 +0700

    update modal category

--- app/screens/product/component/modal-category.tsx
+++ app/screens/product/component/modal-category.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, TouchableOpacity, View } from 'react-native';
 import Modal from 'react-native-modal/dist/modal';
 import { Text } from '../../../components';
-import { colors, padding, scaleHeight, scaleWidth } from '../../../theme';
+import { colors, fontSize, padding, scaleHeight, scaleWidth } from '../../../theme';
 
 const CategoryModalFilter = ({ showCategory, setShowCategory, dataCategory, selectedCategory, setSelectedCategory, setNameDirectory, isSearchBarVisible, setIndex }: any) => (
     <Modal
@@ -16,50 +16,61 @@ const CategoryModalFilter = ({ showCategory, setShowCategory, dataCategory, sele
         style={{
             // flex : 1,
             justifyContent: 'flex-end',
-            margin: 16,
+            margin: 0,
         }}>
-            <View
-                style={{
-                    borderWidth: 1,
-                    borderColor: colors.palette.veryLightGrey,
-                    height: scaleHeight(300),
-                    width: '100%',
-                    backgroundColor: colors.palette.neutral100,
-                }}
-            >
-                <ScrollView>
-                    {dataCategory.map((item: any, index: any) => {
-                        return (
-                            <TouchableOpacity
-                                key={index}
-                                onPress={() => {
-                                    setSelectedCategory(item.id);
-                                    setNameDirectory(item.name);
-                                    setShowCategory(false);
-                                    setIndex(index);
-                                } }
-                                style={{
-                                    paddingVertical: scaleHeight(padding.padding_12),
-                                    paddingHorizontal: scaleWidth(padding.padding_12),
-                                    backgroundColor: selectedCategory === item.id
-                                        ? colors.palette.navyBlue
-                                        : colors.palette.neutral100,
-                                }}>
-                                <Text
-                                    style={{
-                                        fontWeight: "500",
-                                        fontSize: 10, // Adjust font size accordingly
-                                        color: selectedCategory === item.id
-                                            ? colors.palette.neutral100
-                                            : colors.palette.nero,
-                                    }}>
-                                    {item.name}
-                                </Text>
-                            </TouchableOpacity>
-                        );
-                    })}
-                </ScrollView>
+        <View
+            style={{
+                borderWidth: 1,
+                borderColor: colors.palette.veryLightGrey,
+                maxHeight: '40%',
+                width: '100%',
+                backgroundColor: colors.palette.neutral100,
+                borderTopLeftRadius: 16, borderTopRightRadius: 16
+            }}
+        >
+            <View style={{ paddingVertical: scaleHeight(12), paddingHorizontal: scaleWidth(16) }}>
+                <Text tx={'inforMerchant.Category'} style={{
+                    fontWeight: '700', fontSize: fontSize.size14,
+                    lineHeight: scaleHeight(24),
+                    color: colors.palette.nero,
+                }} />
             </View>
+            <ScrollView style={{
+                borderTopLeftRadius: 16,
+                borderTopRightRadius: 16,
+            }}>
+                {dataCategory.map((item: any, index: any) => {
+                    return (
+                        <TouchableOpacity
+                            key={index}
+                            onPress={() => {
+                                setSelectedCategory(item.id);
+                                setNameDirectory(item.name);
+                                setShowCategory(false);
+                                setIndex(index);
+                            }}
+                            style={{
+                                paddingVertical: scaleHeight(padding.padding_12),
+                                paddingHorizontal: scaleWidth(padding.padding_16),
+                                backgroundColor: selectedCategory === item.id
+                                    ? colors.palette.navyBlue
+                                    : colors.palette.neutral100,
+                            }}>
+                            <Text
+                                style={{
+                                    fontWeight: "500",
+                                    fontSize: 10, // Adjust font size accordingly
+                                    color: selectedCategory === item.id
+                                        ? colors.palette.neutral100
+                                        : colors.palette.nero,
+                                }}>
+                                {item.name}
+                            </Text>
+                        </TouchableOpacity>
+                    );
+                })}
+            </ScrollView>
+        </View>
     </Modal>
 );
 
